15d76bd881af11a872622ce0415cd333074832ff
殷春雨 <yinchunyu@jf.com>
2019-04-11 09:55:52 +0800

    'v1.0.0'

---
+++ files-copy.js
@@ -0,0 +1,76 @@
+var fs = require('fs'),
+  stat = fs.stat
+
+/*
+ * 复制目录中的所有文件包括子目录
+ * @param{ String } 需要复制的目录
+ * @param{ String } 复制到指定的目录
+ */
+var _copy = function (src, dst) {
+  // 读取目录中的所有文件/目录
+  fs.readdir(src, function (err, paths) {
+    if (err) {
+      throw err
+    }
+    paths.forEach(function (path) {
+      var _src = src + '/' + path,
+        _dst = dst + '/' + path,
+        readable, writable
+      stat(_src, function (err, st) {
+        if (err) {
+          throw err
+        }
+        // 判断是否为文件
+        if (st.isFile()) {
+          // 创建读取流
+          readable = fs.createReadStream(_src)
+          // 创建写入流
+          writable = fs.createWriteStream(_dst)
+          // 通过管道来传输流
+          readable.pipe(writable)
+        }
+        // 如果是目录则递归调用自身
+        else if (st.isDirectory()) {
+          copy(_src, _dst, _copy)
+        }
+      })
+    })
+  })
+}
+// 在复制目录前需要判断该目录是否存在，不存在需要先创建目录
+var copy = function (src, dst) {
+  fs.exists(dst, function (exists) {
+    // 已存在
+    if (exists) {
+      _copy(src, dst)
+    }
+    // 不存在
+    else {
+      fs.mkdir(dst, function () {
+        _copy(src, dst)
+      })
+    }
+  })
+}
+
+// 删除文件
+function deleteall(path) {
+  var files = []
+  if (fs.existsSync(path)) {
+    files = fs.readdirSync(path)
+    files.forEach(function (file, index) {
+      var curPath = path + "/" + file
+      if (fs.statSync(curPath).isDirectory()) { // recurse
+        deleteall(curPath)
+      } else { // delete file
+        fs.unlinkSync(curPath)
+      }
+    })
+    fs.rmdirSync(path)
+  }
+}
+
+module.exports = {
+  deleteall,
+  copy
+}
